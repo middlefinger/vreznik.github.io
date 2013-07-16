2fc3918888d5118efebeebd3a263e2aa4813740f
middlefinger <lefinger@yandex.com>
2013-07-17 02:46:11 +0300

    almost

--- testing/jsanddom.js
+++ testing/jsanddom.js
@@ -6,17 +6,25 @@
 
      // Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
-         // FILL THIS IN
+		 return str.split('').reverse().join('');
      }
 
      // Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
-         // FILL THIS IN
+		 return Math.min.apply(Math, values);
      }
 
      // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
-     function findDistinctValues(values) {
-         // FILL THIS IN
+     function findDistinctValues(values, qty) {
+		values.sort();
+        for(var i = 1; i < values.length; ){
+            if(values[i-1] == values[i]){
+                values.splice(i - (qty - 1 || 0), (qty || 1));
+            } else {
+                i++;
+            }
+        }
+        return values;
      }
 
      // Write a function that logs the numbers from 1 to 100 to the console.
@@ -24,7 +32,15 @@
      // For multiples of five print "Buzz".
      // For numbers which are multiples of both three and five print "FizzBuzz".
      function doFizzBuzz() {
-         // FILL THIS IN
+         if(typeof console === 'object'){
+			for(var i = 1, temp; i < 101; i++){
+				temp = i;
+				if(i % 3 == 0 && i % 5 == 0) temp = 'FizzBuzz';
+				else if(i % 3 == 0) temp = 'Fizz';
+				else if(i % 5 == 0) temp = 'Buzz';
+				console.log(temp);
+			}
+		 }
      }
 
      // You have a master array of strings, where each element is a fruit name.
@@ -32,7 +48,7 @@
      // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
      // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
      function removeFruits(fruits, fruitsToRemove) {
-         // FILL THIS IN
+         return findDistinctValues(fruits.concat(fruitsToRemove), 2);
      }
 
      // Write a function to push either a simple value or an array of values onto a specified array.
@@ -40,25 +56,35 @@
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
      function pushOntoArray(array, toPush) {
-         // FILL THIS IN
+		 array.push[typeof toPush === 'object' ? 'apply' : 'call'](array, toPush);
+         return array;
      }
 
      // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
-         // FILL THIS IN
+         return sourceStr != '' ? sourceStr.split(',') : [];
      }
 
      // Write a function that will take any number of arguments and return their sum
      function sum() {
-         // FILL THIS IN
+		for (var i = 0, sum = 0, l = arguments.length; i < l; i++) {
+			sum += arguments[i];
+		}
+		return sum;
      }
 
      // Write a function that will return true if a specified string consists of only whitespace.
      function isOnlyWhitespace(sourceStr) {
-         // FILL THIS IN
+         return /\s/.test(sourceStr);
      }
 
      // write an example of a javascript closure
+	 var incCalls = (function(){
+		var calls = 0;
+		return function(){
+			console.log(++calls);
+		}
+	 })();
 
      // define a json object that represents a collection of people.
      // each person should have the following properties
@@ -68,6 +94,44 @@
      // - state
      // - zip
      // - a collection of phone numbers (home, work, mobile)
+	 [
+		{
+			"firstName": "first name",
+			"lastName": "last name",
+			"city": "City",
+			"state": "Some state",
+			"zip": "12345",
+			"phones": {
+				"home": "+123456",
+				"work": "+234567",
+				"mobile": "+3456789"
+			}
+		},
+		{
+			"firstName": "first name",
+			"lastName": "last name",
+			"city": "City",
+			"state": "Some state",
+			"zip": "12345",
+			"phones": {
+				"home": "+123456",
+				"work": "+234567",
+				"mobile": "+3456789"
+			}
+		},
+		{
+			"firstName": "first name",
+			"lastName": "last name",
+			"city": "City",
+			"state": "Some state",
+			"zip": "12345",
+			"phones": {
+				"home": "+123456",
+				"work": "+234567",
+				"mobile": "+3456789"
+			}
+		}
+	 ];
 
 
      // Create a javascript object (DataTable) with the following:
@@ -75,12 +139,30 @@
      // A private rows property (string array)
      // A public method addRows that adds an item to the rows array
      // A public method addColumns that adds an item to the columns array
-     // A public method getData that returns the a json object representation of the DataTable
+     // A public method getData tthat returns the a json object representation of the DataTable
      // Note: the row object should be a hash of the column name and row item value
      // Example:
      // .addColumns('column1', 'column2', 'column3');
      // .addRow('value1A', 'value1B', 'value1C');
      // .addRow('value2A', 'value2B', 'value2C');
+	 var DataTable = (function(){
+		var rows = [],
+			columns = [];
+			
+		return {
+			addRows: function(){
+				rows.push.apply(rows, arguments);
+			},
+			addColumns: function(){
+				columns.push.apply(columns, arguments);
+			},
+			getData: function(){
+				return {
+					
+				}
+			}
+		};
+	 })();
 
      // within div1, programatically create a
      // SELECT element (with multiple items) and a button.
